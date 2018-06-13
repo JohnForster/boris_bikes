045fb5e5e70cf4e36c962346b0e54e9a6be2f961
John Forster <j_forster@hotmail.co.uk>
2018-06-13 15:44:48 +0100

    Halfway through challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,16 @@ require './lib/bike.rb'
 
 # The docking station for bikes in the Boris Bike project.
 class DockingStation
+  # def initialize
+  #   @bikes = []
+  #   5.times { @bikes << Bike.new }
+  # end
+
+  attr_accessor :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike); end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,23 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
+  before do
+  end
+
   docking_station = DockingStation.new
+  bike = docking_station.release_bike
+
   it { should respond_to(:release_bike) }
-  it 'should be able to release a working bike' do
-    bike = docking_station.release_bike
+  it 'should be able to release a bike' do
     expect(bike.class).to eq Bike
+  end
+  it 'the released bike should be working' do
     expect(bike.working?).to eq true
+  end
+  it 'should respond to docking' do
+    docking_station.should respond_to(:dock).with(1).argument
+  end
+  it 'should be able to list docked bikes' do
+    docking_station.should respond_to(:bike)
   end
 end
